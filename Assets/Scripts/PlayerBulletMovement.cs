3f8fb5df95b7424176f00ad4100c1ec902245fa2
MarcusBoay <boay@ualberta.ca>
2016-12-02 23:10:06 -0700

    Bullet change
    Offset player bullet spawn position
    Made public variable for bullet speed

--- Assets/Scripts/PlayerBulletMovement.cs
+++ Assets/Scripts/PlayerBulletMovement.cs
@@ -6,12 +6,15 @@ public class PlayerBulletMovement : MonoBehaviour
 
     private Rigidbody2D rb2d;
     private GameObject player;
+    public float offsetX;
+    public float offsetY;
+    public float speed;
     
     void Start()
     {
         player = GameObject.Find("Player");
         rb2d = GetComponent<Rigidbody2D>();
-        transform.position = player.transform.position;
-        rb2d.velocity = new Vector3(1, 0, 0);
+        transform.position = player.transform.position + new Vector3(1, 0, 0) * offsetX + new Vector3(0, 1, 0) * offsetY;
+        rb2d.velocity = new Vector3(1, 0, 0) * speed;
     }
 }
