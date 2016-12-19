1d70c6b815aaf532433ebdac87781fbb70edf83f
MarcusBoay <boay@ualberta.ca>
2016-12-19 00:13:17 -0700

    Updated bug list

--- Assets/Bugs.cs
+++ Assets/Bugs.cs
@@ -5,4 +5,6 @@ public class Bugs : MonoBehaviour
 {
     //1) player bullet loses velocity when player is killed & error message pops up (CANNOT BE RECREATED)
     //2) player's consecutive bullets not equal distance
+    //3) when 2 directional buttons are pressed at the same time, player does not move (but only sometimes?)
+    //4) player bullets appear for split second at where it was deactivated
 }
