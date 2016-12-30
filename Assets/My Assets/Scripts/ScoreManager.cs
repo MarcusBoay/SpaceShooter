3b238134b02914365eb10b8eeb94d6e3cdcb05de
MarcusBoay <boay@ualberta.ca>
2016-12-30 02:02:43 -0700

    Imported main BGM audio

--- Assets/My Assets/Scripts/ScoreManager.cs
+++ Assets/My Assets/Scripts/ScoreManager.cs
@@ -1,7 +1,19 @@
 using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class ScoreManager : MonoBehaviour 
 {
     public int score;
+    public GameObject scoreKeeper;
+
+    void Update()
+    {
+        SetScore();
+    }
+
+    void SetScore()
+    {
+        scoreKeeper.GetComponent<Text>().text = score.ToString();
+    }
 }
